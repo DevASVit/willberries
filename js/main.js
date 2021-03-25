371e5f425921f47386d3a9e5e1a61ff25268286a
DevASVit <devasvit@gmail.com>
2021-03-25 14:30:47 +0200

    add open/close cart and smooth scroll

--- js/main.js
+++ js/main.js
@@ -8,3 +8,39 @@ const mySwiper = new Swiper('.swiper-container', {
 	},
 });
 
+// Cart
+const buttonCart = document.querySelector('.button-cart');
+const modalCart = document.querySelector('#modal-cart');
+
+const openModal = () => {
+	modalCart.classList.add('show');
+};
+
+const closeModal = () => {
+	modalCart.classList.remove('show');
+};
+
+buttonCart.addEventListener('click', openModal);
+
+modalCart.addEventListener('click', event => {
+	const target = event.target;
+	if (target.classList.contains('overlay') || target.classList.contains('modal-close')) {
+		closeModal();
+	}
+});
+
+// Scroll Smooth
+{
+	const scrollLinks = document.querySelectorAll('a.scroll-link');
+
+	for (let i = 0; i < scrollLinks.length; i++) {
+		scrollLinks[i].addEventListener('click', event => {
+			event.preventDefault();
+			const id = scrollLinks[i].getAttribute('href');
+			document.querySelector(id).scrollIntoView({
+				behavior: 'smooth',
+				block: 'start',
+			});
+		});
+	}
+}
